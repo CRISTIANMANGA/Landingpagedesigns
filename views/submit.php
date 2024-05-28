d7fa57cf01cbf115d8b0369aa7ea6c91131c11bd
CRISTIANMANGA <160682767+CRISTIANMANGA@users.noreply.github.com>
2024-05-28 23:23:32 +0000

    feat :rocket: implemente imagenes a beaty

---
+++ views/submit.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $to = 'tudirecciondecorreo@example.com'; // Reemplaza con tu dirección de correo electrónico
+    $subject = 'Mensaje de tu sitio web';
+    $body = "Nombre: $name\nCorreo electrónico: $email\nMensaje:\n$message";
+
+    if (mail($to, $subject, $body)) {
+        echo 'Mensaje enviado con éxito. ¡Gracias por contactarnos!';
+    } else {
+        echo 'Hubo un problema al enviar el mensaje. Por favor, inténtalo de nuevo más tarde.';
+    }
+}
+?>
